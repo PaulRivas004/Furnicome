361c072f0f5e877ac11c61f0e9baae3ba0768a93
z1trofer <fejoorpo@gmail.com>
2023-04-10 11:47:52 -0600

    carpetas y parte de php  para el login

---
+++ Furnicome/controllers/dashboard/index.js
@@ -0,0 +1,14 @@
+LOGIN_FORM.addEventListener('submit', async (event) => {
+    // Se evita recargar la página web después de enviar el formulario.
+    event.preventDefault();
+    // Constante tipo objeto con los datos del formulario.
+    const FORM = new FormData(LOGIN_FORM);
+    // Petición para iniciar sesión.
+    const JSON = await dataFetch(USER_API, 'login', FORM);
+    // Se comprueba si la respuesta es satisfactoria, de lo contrario se muestra un mensaje con la excepción.
+    if (JSON.status) {
+        sweetAlert(1, JSON.message, true, 'main.html');
+    } else {
+        sweetAlert(2, JSON.exception, false);
+    }
+});
