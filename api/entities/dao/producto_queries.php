aa1d8503777c49c731a947f2e181eb733f466b17
z1trofer <fejoorpo@gmail.com>
2023-04-22 20:54:20 -0600

    crud de productos
    no le sirve la imagen pero todo bien

--- api/entities/dao/producto_queries.php
+++ api/entities/dao/producto_queries.php
@@ -14,13 +14,13 @@ class ProductoQueries
     {
         $sql = 'INSERT INTO productos( id_subcategoria, id_usuario, nombre_producto, descripcion_producto, precio_producto, imagen_producto, estado_producto, existencia_producto)
         VALUES ( ?, ?, ?, ?, ?, ?, ?, ?)';
-        $params = array($this->id_subcategoria, $this->id_usuario, $this->nombre_producto, $this->descripcion_producto, $this->precio_producto, $this->imagen_producto, $this ->estado_producto, $this->existencia_producto, $_SESSION['id_usuario']);
+        $params = array($this->id_subcategoria, $this->id_usuario, $this->nombre_producto, $this->descripcion_producto, $this->precio_producto, $this->imagen_producto, $this ->estado_producto, $this->existencia_producto);
         return Database::executeRow($sql, $params);
     }
 
     public function readAll()
     {
-        $sql = 'SELECT id_producto, imagen_producto,nombre_usuario, nombre_producto, descripcion_producto, precio_producto, nombre_sub, estado_producto, existencia_producto
+        $sql = 'SELECT *
         FROM productos INNER JOIN subcategorias USING(id_subcategoria)
         INNER JOIN usuarios USING(id_usuario)
         ORDER BY nombre_producto';
@@ -29,22 +29,22 @@ class ProductoQueries
 
     public function readOne()
     {
-        $sql = 'SELECT id_producto, nombre_producto, descripcion_producto, precio_producto, imagen_producto, id_categoria, estado_producto
+        $sql = 'SELECT *
                 FROM productos
                 WHERE id_producto = ?';
-        $params = array($this->id);
+        $params = array($this->id_producto);
         return Database::getRow($sql, $params);
     }
 
     public function updateRow($current_image)
     {
         // Se verifica si existe una nueva imagen para borrar la actual, de lo contrario se mantiene la actual.
-        ($this->imagen) ? Validator::deleteFile($this->getRuta(), $current_image) : $this->imagen = $current_image;
+        ($this->imagen_producto) ? Validator::deleteFile($this->getRuta(), $current_image) : $this->imagen = $current_image;
 
         $sql = 'UPDATE productos
-                SET imagen_producto = ?, nombre_producto = ?, descripcion_producto = ?, precio_producto = ?, estado_producto = ?, id_categoria = ?
+                SET id_subcategoria = ?, id_usuario = ?, nombre_producto = ?, descripcion_producto = ?, precio_producto = ?, imagen_producto = ?, estado_producto = ?, existencia_producto = ?
                 WHERE id_producto = ?';
-        $params = array($this->imagen, $this->nombre, $this->descripcion, $this->precio, $this->estado, $this->categoria, $this->id);
+        $params = array($this->id_subcategoria, $this->id_usuario, $this->nombre_producto, $this->descripcion_producto, $this->precio_producto, $this ->imagen_producto, $this->estado_producto, $this->existencia_producto, $this->id_producto);
         return Database::executeRow($sql, $params);
     }
 
@@ -52,9 +52,19 @@ class ProductoQueries
     {
         $sql = 'DELETE FROM productos
                 WHERE id_producto = ?';
-        $params = array($this->id);
+        $params = array($this->id_producto);
         return Database::executeRow($sql, $params);
     }
 
- 
+    public function readSub(){
+        $sql = 'SELECT id_subcategoria, nombre_sub, descripcion_sub, imagen, id_categoria
+                FROM subcategorias';
+        return Database::getRows($sql);
+    }
+
+    public function cargarUsuario(){
+        $sql = 'SELECT id_usuario, nombre_usuario, apellido_usuario, correo_usuario, alias_usuario, clave_usuario
+                FROM usuarios';
+        return Database::getRows($sql);
+    }
 }
---
+++ controllers/dashboard/producto.js
@@ -0,0 +1,150 @@
+// Constante para completar la ruta de la API.
+const PRODUCTO_API = 'business/dashboard/producto.php';
+// Constante para establecer el formulario de buscar.
+const SEARCH_FORM = document.getElementById('search-form');
+// Constante para establecer el formulario de guardar.
+const SAVE_FORM = document.getElementById('save-form');
+// Constante para establecer el título de la modal.
+const MODAL_TITLE = document.getElementById('modal-title');
+// Constantes para establecer el contenido de la tabla.
+const TBODY_ROWS = document.getElementById('tbody-rows');
+const RECORDS = document.getElementById('records');
+// Constante tipo objeto para establecer las opciones del componente Modal.
+const OPTIONS = {
+    dismissible: false
+}
+
+// Método manejador de eventos para cuando el documento ha cargado.
+document.addEventListener('DOMContentLoaded', () => {
+    // Llamada a la función para llenar la tabla con los registros disponibles.
+    fillTable();
+});
+
+// Método manejador de eventos para cuando se envía el formulario de guardar.
+SAVE_FORM.addEventListener('submit', async (event) => {
+    // Se evita recargar la página web después de enviar el formulario.
+    event.preventDefault();
+    // Se verifica la acción a realizar.
+    (document.getElementById('id').value) ? action = 'update' : action = 'create';
+    // Constante tipo objeto con los datos del formulario.
+    const FORM = new FormData(SAVE_FORM);
+    // Petición para guardar los datos del formulario.
+    const JSON = await dataFetch(PRODUCTO_API, action, FORM);
+    // Se comprueba si la respuesta es satisfactoria, de lo contrario se muestra un mensaje con la excepción.
+    if (JSON.status) {
+        // Se carga nuevamente la tabla para visualizar los cambios.
+        fillTable();
+
+        // Se muestra un mensaje de éxito.
+        sweetAlert(1, JSON.message, true);
+    } else {
+        sweetAlert(2, JSON.exception, false);
+    }
+});
+
+/*
+*   Función asíncrona para llenar la tabla con los registros disponibles.
+*   Parámetros: form (objeto opcional con los datos de búsqueda).
+*   Retorno: ninguno.
+*/
+async function fillTable(form = null) {
+    // Se inicializa el contenido de la tabla.
+    TBODY_ROWS.innerHTML = '';
+    RECORDS.textContent = '';
+    // Se verifica la acción a realizar.
+    (form) ? action = 'search' : action = 'readAll';
+    // Petición para obtener los registros disponibles.
+    const JSON = await dataFetch(PRODUCTO_API, action, form);
+    // Se comprueba si la respuesta es satisfactoria, de lo contrario se muestra un mensaje con la excepción.
+    if (JSON.status) {
+        // Se recorre el conjunto de registros fila por fila.
+        JSON.dataset.forEach(row => {
+            // Se crean y concatenan las filas de la tabla con los datos de cada registro.
+            TBODY_ROWS.innerHTML += `
+                <tr>
+                    <td>${row.nombre_sub}</td>
+                    <td>${row.nombre_usuario}</td>
+                    <td>${row.nombre_producto}</td>
+                    <td>${row.descripcion_producto}</td>
+                    <td>${row.precio_producto}</td>
+                    <td>${row.imagen_producto}</td>
+                    <td>${row.estado_producto}</td>
+                    <td>${row.existencia_producto}</td>
+                    <td>
+                    <button onclick="openUpdate(${row.id_producto})" type="button" class="btn btn-primary" data-mdb-toggle="modal" data-mdb-target=".exampleModal">
+                    Actualizar
+                    </button>
+                    <button onclick="openDelete(${row.id_producto})" type="button" class="btn btn-danger">Eliminar</button>
+                    </td>
+                </tr>
+            `;
+        });
+        RECORDS.textContent = JSON.message;
+    } else {
+        sweetAlert(4, JSON.exception, true);
+    }
+}
+
+/*
+*   Función para preparar el formulario al momento de insertar un registro.
+*   Parámetros: ninguno.
+*   Retorno: ninguno.
+*/
+function openCreate() {
+    // Se restauran los elementos del formulario.
+    SAVE_FORM.reset();
+    // Se establece el campo de archivo como obligatorio.
+    document.getElementById('archivo').required = true;
+    // Llamada a la función para llenar el select del formulario. Se encuentra en el archivo components.js
+    fillSelect(PRODUCTO_API, 'readSub', 'subcategoria');
+    fillSelect(PRODUCTO_API, 'cargarUsuario', 'usuario');
+}
+
+async function openUpdate(id) {
+    const FORM = new FormData();
+    FORM.append('id', id);
+    const JSON = await dataFetch(PRODUCTO_API, 'readOne', FORM);
+    
+    if (JSON.status) {
+        document.getElementById('id').value = JSON.dataset.id_producto;
+        fillSelect(PRODUCTO_API, 'readSub', 'subcategoria', JSON.dataset.id_subcategoria);
+        fillSelect(PRODUCTO_API, 'cargarUsuario', 'usuario', JSON.dataset.id_usuario);
+        document.getElementById('nombre').value = JSON.dataset.nombre_producto;
+        document.getElementById('descripcion').value = JSON.dataset.descripcion_producto;
+        document.getElementById('precio').value = JSON.dataset.precio_producto;
+        document.getElementById('archivo').required = false;
+        if (JSON.dataset.estado_producto) {
+            document.getElementById('estados').checked = true;
+        } else {
+            document.getElementById('estados').checked = false;
+        }
+        document.getElementById('existencia').value = JSON.dataset.existencia_producto;
+    }
+}
+
+/*
+*   Función asíncrona para eliminar un registro.
+*   Parámetros: id (identificador del registro seleccionado).
+*   Retorno: ninguno.
+*/
+async function openDelete(id) {
+    // Llamada a la función para mostrar un mensaje de confirmación, capturando la respuesta en una constante.
+    const RESPONSE = await confirmAction('¿Desea eliminar el producto de forma permanente?');
+    // Se verifica la respuesta del mensaje.
+    if (RESPONSE) {
+        // Se define una constante tipo objeto con los datos del registro seleccionado.
+        const FORM = new FormData();
+        FORM.append('id_producto', id);
+        // Petición para eliminar el registro seleccionado.
+        const JSON = await dataFetch(PRODUCTO_API, 'delete', FORM);
+        // Se comprueba si la respuesta es satisfactoria, de lo contrario se muestra un mensaje con la excepción.
+        if (JSON.status) {
+            // Se carga nuevamente la tabla para visualizar los cambios.
+            fillTable();
+            // Se muestra un mensaje de éxito.
+            sweetAlert(1, JSON.message, true);
+        } else {
+            sweetAlert(2, JSON.exception, false);
+        }
+    }
+}
